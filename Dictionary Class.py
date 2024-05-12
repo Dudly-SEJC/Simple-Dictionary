2eca6300c662ef82c653f624f5cb60ac2407c7bf
Sebastian C <161907841+Dudly-SEJC@users.noreply.github.com>
2024-05-11 20:51:36 -0700

    Add files via upload

---
+++ Dictionary Class.py
@@ -0,0 +1,10 @@
+#Create a dictionary
+my_dict={
+    "BUS308":"Section A",
+    "BUS472":"SP24_1",
+    "BUS468":"SP24",
+    "Year":2024
+}
+print(my_dict)
+mydict=dict(my_dict) #Making a copy of dictionary
+
